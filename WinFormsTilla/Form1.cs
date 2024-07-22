de9011d2432715d4e38542caf809d884aabd79fc
Brandon <brandon139@protonmail.com>
2024-07-22 23:23:29 +1000

    code folding

--- WinFormsTilla/Form1.cs
+++ WinFormsTilla/Form1.cs
@@ -16,6 +16,34 @@ public partial class Form1 : Form
         this.Controls.Add(scintilla);
         scintilla.Dock = DockStyle.Fill;
         SetupCSharpSyntaxHighlighting(scintilla);
+
+        scintilla.SetProperty("fold", "1");
+        scintilla.SetProperty("fold.compact", "1");
+
+        // Configure a margin to display folding symbols
+        scintilla.Margins[2].Type = MarginType.Symbol;
+        scintilla.Margins[2].Mask = Marker.MaskFolders;
+        scintilla.Margins[2].Sensitive = true;
+        scintilla.Margins[2].Width = 20;
+
+        // Set colors for all folding markers
+        for (int i = 25; i <= 31; i++)
+        {
+            scintilla.Markers[i].SetForeColor(SystemColors.ControlLightLight);
+            scintilla.Markers[i].SetBackColor(SystemColors.ControlDark);
+        }
+
+        // Configure folding markers with respective symbols
+        scintilla.Markers[Marker.Folder].Symbol = MarkerSymbol.BoxPlus;
+        scintilla.Markers[Marker.FolderOpen].Symbol = MarkerSymbol.BoxMinus;
+        scintilla.Markers[Marker.FolderEnd].Symbol = MarkerSymbol.BoxPlusConnected;
+        scintilla.Markers[Marker.FolderMidTail].Symbol = MarkerSymbol.TCorner;
+        scintilla.Markers[Marker.FolderOpenMid].Symbol = MarkerSymbol.BoxMinusConnected;
+        scintilla.Markers[Marker.FolderSub].Symbol = MarkerSymbol.VLine;
+        scintilla.Markers[Marker.FolderTail].Symbol = MarkerSymbol.LCorner;
+
+        // Enable automatic folding
+        scintilla.AutomaticFold = (AutomaticFold.Show | AutomaticFold.Click | AutomaticFold.Change);
     }
 
 
